8a3aad7a3fc453feebc05dd0979d071312a883a2
letoan <letranthetoan@gmail.com>
2017-02-17 12:54:48 +0700

    first checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'contact_us' => 'home#contact_us'
+  get 'menu' => 'home#menu'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "home#index"
 end
